80ada99063d61b9356acf48e8acf4200a4d8165b
Rohit Kumar <rohit@gmail.com>
2025-01-01 01:53:13 +0530

    sfsdf

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // Automatically opens the app in the browser
+    open: false, 
+    port: 8000 
   },
 });
